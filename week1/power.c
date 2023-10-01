32de2b71d14ed5c0b0f17ef2bcb08c1acd64af9e
amigler <amigler@calpoly.edu>
2023-10-01 23:21:10 +0000

    week1/week2 code

---
+++ week1/power.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int power(int base, int n);
+
+int main(int argc, char *argv[]) {
+    printf("power(2, 5) is: %d\n", power(2, 5));
+    return 0; // success
+}
+
+// function definition
+// power: raise base to n-th power; n >= 0
+int power(int base, int n) {
+    int i, p;
+    p = 1;
+    for (i = 1; i <= n; ++i) {
+        p = p * base;
+    }
+    return p;
+}
