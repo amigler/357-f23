7a5ca939e496b1dabf5468ccb26de05f9dc5ba47
amigler <amigler@calpoly.edu>
2023-11-06 16:52:50 +0000

    exec example

--- week7/exec_ls.c
+++ week7/exec_ls.c
@@ -2,15 +2,26 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <fcntl.h>
 
 int main(void) {
 
-    printf("before\n");
-    
+    printf("before exec\n");
+
     char *my_argv[] = { "ls", "-l", (char*) 0 };
+    char *my_env[] = { "PATH=.", "USER=me", (char*) 0 };
+    
     if (execv("/bin/ls", my_argv) < 0) {
-        printf("execlp error");
+        printf("execv error");
     }
+
+    /*
+    int ls_fd = open("/bin/ls", O_RDONLY);
+    if (fexecve(ls_fd, my_argv, my_env) < 0) {
+        printf("fexecve error");
+    }
+    */
+    
     // process never reaches this line (unless exec fails)
     printf("after\n");
 }
